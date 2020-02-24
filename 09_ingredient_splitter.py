c70eceb09a918fe4d539385ead39680f176e7251
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-02-25 12:14:53 +1300

    ingredient splliter - identifies mixed fractions

--- 09_ingredient_splitter.py
+++ 09_ingredient_splitter.py
@@ -7,5 +7,22 @@ mixed_regex = "\d{1,3}\s\d{1,3}\/\d{1,3}"
 
 if re.match(mixed_regex, recipe_line):
     print("true")
-else:
-    print("false")
+    # Get mixed number by matching the regex
+    pre_mixed_num = re.match(mixed_regex, recipe_line)
+    mixed_num = pre_mixed_num.group()
+
+    # Replace space with a + sign...
+    amount = mixed_num.replace(" ", "+")
+    # Change the string into a decimal
+    amount = eval(amount)
+    print(amount)
+
+    # Get unit and ingredient
+    compile_regex = re.compile(mixed_regex)
+    print(compile_regex)
+    unit_ingredient = re.split(compile_regex, recipe_line)
+    unit_ingredient = (unit_ingredient[1]).strip() # remove extra white space from unit
+    print(unit_ingredient)
+
+get_unit = unit_ingredient.split(" ", 1) # splits text at first space
+print(get_unit)
