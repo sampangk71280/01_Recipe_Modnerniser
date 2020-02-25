0874e1a73fbafad6e67481d5b1209c509d564a26
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-02-26 10:55:11 +1300

    partially working ingredient splitter (video pt3)

--- 09_ingredient_splitter.py
+++ 09_ingredient_splitter.py
@@ -1,28 +1,43 @@
 import re
 
 # ingredient has mixed fraction followed by unit and ingredient
-recipe_line = "1 1/2 ml flour" # Change to input statement in due course
+
+full_recipe = [
+    "1 1/2 ml flour",
+    "3/4 cup milk",
+    " 1 cup flour",
+    "2 tablespoons white sugar",
+    "1.5 tsp baking powder",
+    "pinch of cinammon"
+]
 
 mixed_regex = "\d{1,3}\s\d{1,3}\/\d{1,3}"
 
-if re.match(mixed_regex, recipe_line):
-    print("true")
-    # Get mixed number by matching the regex
-    pre_mixed_num = re.match(mixed_regex, recipe_line)
-    mixed_num = pre_mixed_num.group()
+for recipe_line in full_recipe:
+    # Get amount...
+    if re.match(mixed_regex, recipe_line):
+        print("true")
+        # Get mixed number by matching the regex
+        pre_mixed_num = re.match(mixed_regex, recipe_line)
+        mixed_num = pre_mixed_num.group()
 
-    # Replace space with a + sign...
-    amount = mixed_num.replace(" ", "+")
-    # Change the string into a decimal
-    amount = eval(amount)
-    print(amount)
+        # Replace space with a + sign...
+        amount = mixed_num.replace(" ", "+")
+        # Change the string into a decimal
+        amount = eval(amount)
+        print(amount)
 
-    # Get unit and ingredient
-    compile_regex = re.compile(mixed_regex)
-    print(compile_regex)
-    unit_ingredient = re.split(compile_regex, recipe_line)
-    unit_ingredient = (unit_ingredient[1]).strip() # remove extra white space from unit
-    print(unit_ingredient)
+        # Get unit and ingredient
+        compile_regex = re.compile(mixed_regex)
+        print(compile_regex)
+        unit_ingredient = re.split(compile_regex, recipe_line)
+        unit_ingredient = (unit_ingredient[1]).strip() # remove extra white space from unit
+        print(unit_ingredient)
 
-get_unit = unit_ingredient.split(" ", 1) # splits text at first space
-print(get_unit)
+    get_unit = unit_ingredient.split(" ", 1) # splits text at first space
+    print(get_unit)
+
+    unit = get_unit[0]
+    ingredient = get_unit[1]
+    print(unit)
+    print(ingredient)
